703a78fb75791ab1e9c5ce13f79962230daac782
Victor Garcia <victor.g@uteca.edu.mx>
2024-03-19 07:44:33 -0600

    lectura de xlsx

--- lib/myfunc.py
+++ lib/myfunc.py
@@ -1,11 +1,18 @@
-from lib import *
+#import openpyxl 
 
 
-Número = int(input ("¿Cuál es mi número? " ))
-NombreArchivo = input ("¿Cuál es el nombre del archivo a procesar? " )
+#def datosEval (datos):
+#    workbook = openpyxl.load_workbook (datos)
+#    hoja = workbook.active
+#    data = [ ]
+#    for row in hoja.iter_rows (values_only = True):
+#        data.append(row)
+#    return data
 
-#datosEval (NombreArchivo)
 
-print ("")
+def Datos (datosEval):    
+    archivo = open((datosEval),'r')
+    contenido = archivo.read ()
+    print (contenido)
+    archivo.close()
 
-Datos (NombreArchivo)
